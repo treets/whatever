ea5384c71f27b542a58a7f2aa1aa95bb17a13e8d
treets <mail@andreas-theis.net>
2020-02-14 10:39:53 +0100

    Updates.

--- main.ts
+++ main.ts
@@ -1,12 +1,22 @@
 namespace handling {
-    //% block="move %kin to positions %pos at %vel with %acc acceleration jumping from %start to %end max %max"
+    /**
+     * A simple pick and place movement.
+     * @param kin the kinematic object to be used
+     * @param pos the position to be reached
+     * @param vel the max velocity
+     * @param acc acceleration and deceleration
+     * @param start the minimum height to be reached before blending for the first movement
+     * @param end the minimum height to be reached after blending for the third movement
+     * @param max the total maximum height of movement
+     */
+    //% block="jump %kin to positions %pos at %vel with %acc acceleration blending from %startHeight to %endHeight limited by %maxHeight"
     //% inlineInputMode=inline
-    export function moveJump(kin: motion.Kinematic, pos: number[], vel: number, acc: number, start: number, end: number, max: number) {
+    export function moveJumpAbsolute(kin: motion.Kinematic, pos: number[], vel: number, acc: number, startHeight: number, endHeight: number, maxHeight: number) {
         const current = kin.values();
-        MotionLib.kinCmdBlend(kin.getName(), max - start, max - start);
-        MotionLib.kinCmdMoveLinAbs(kin.getName(), [current[0], current[1], current[2] + max], vel, acc, acc, 0, 0);
-        MotionLib.kinCmdBlend(kin.getName(), max - end, max - end);
-        MotionLib.kinCmdMoveLinAbs(kin.getName(), [pos[0], pos[1], pos[2] + max], vel, acc, acc, 0, 0);
+        MotionLib.kinCmdBlend(kin.getName(), maxHeight - startHeight, maxHeight - startHeight);
+        MotionLib.kinCmdMoveLinAbs(kin.getName(), [current[0], current[1], current[2] + maxHeight], vel, acc, acc, 0, 0);
+        MotionLib.kinCmdBlend(kin.getName(), maxHeight - endHeight, maxHeight - endHeight);
+        MotionLib.kinCmdMoveLinAbs(kin.getName(), [pos[0], pos[1], pos[2] + maxHeight], vel, acc, acc, 0, 0);
         MotionLib.kinCmdMoveLinAbs(kin.getName(), pos, vel, acc, acc, 0, 0);
         // const xDir: AxisDir = current[0] - pos[0] > 0 ? AxisDir.NEGATIVE : AxisDir.POSITIVE;
         // control.pauseUntilAxisPositionPassed(kin.getAxis(AxisMeaning.MAIN_AXIS_X), pos[0], xDir, 0);
