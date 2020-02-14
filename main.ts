68b4d7a94f41cd94403d7530a3ffc3c2f3f8e7f9
treets <mail@andreas-theis.net>
2020-02-14 10:08:33 +0100

    aaa

--- main.ts
+++ main.ts
@@ -4,9 +4,9 @@ namespace handling {
     export function moveJump(kin: motion.Kinematic, pos: number[], vel: number, acc: number, start: number, end: number, max: number) {
         const current = kin.values();
         MotionLib.kinCmdBlend(kin.getName(), max - start, max - start);
-        kin.move(MoveType.ABSOLUTE, [current[0], current[1], current[2] + max], vel, acc);
+        MotionLib.kinCmdMoveLinAbs(kin.getName(), [current[0], current[1], current[2] + max], vel, acc, acc, 0, 0);
         MotionLib.kinCmdBlend(kin.getName(), max - end, max - end);
-        kin.move(MoveType.ABSOLUTE, [pos[0], pos[1], pos[2] + max], vel, acc);
-        kin.move(MoveType.ABSOLUTE, pos, vel, acc);
+        MotionLib.kinCmdMoveLinAbs(kin.getName(), [pos[0], pos[1], pos[2] + max], vel, acc, acc, 0, 0);
+        MotionLib.kinCmdMoveLinAbs(kin.getName(), pos, vel, acc, acc, 0, 0);
     }
 }
